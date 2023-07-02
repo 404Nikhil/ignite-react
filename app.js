a5d11c78ac7e8897696e73bd805ada83baeebe01
404nikhil <nicksd111@gmail.com>
2023-07-02 18:21:24 +0530

    app layout structure done

--- app.js
+++ app.js
@@ -1,58 +1,82 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h1", //diffing algo, recursing on children, react reconcilation
-  },
-  "Heading 1 for Parcel"
-);
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key: "h1", //diffing algo, recursing on children, react reconcilation
+//   },
+//   "Heading 1 for Parcel"
+// );
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-    key: "h2",
-  },
-  "Heading 2"
-);
+// const heading2 = React.createElement(
+//   "h2",
+//   {
+//     id: "title",
+//     key: "h2",
+//   },
+//   "Heading 2"
+// );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-    hello: "world",
-  },
-  [heading, heading2]
-);
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "container",
+//     hello: "world",
+//   },
+//   [heading, heading2]
+// );
 
 // JSX
 // React Component
 // Functional - New , Class based - Old
 
-const heading3 = (
+const title = (
   <h1 id="title" key="h2">
-    Namaste React
+    Food Hub
   </h1>
 );
-const Title = () => (
- <h1 id="heading" key="h3">Hellow</h1>
-)
+// const Title = () => (
+//  <h1 id="heading" key="h3">Hellow</h1>
+// )
 // const data = api.getData(); // JSX prevent hacking (sanitizes js injection by hackers)
 const HeaderComponent = () => { // normal js function, composing components
   return (
     <div>
-     <Title/> 
-     {1+2}
-     {heading3}
+     {title}
+     {
+      //this is a comment
+     }
       <h1>Hellow everynyan</h1>
     </div>
   );
 };
 
-console.log(heading);
+const AppLayout = () => {
+  return (
+    {
+      /** 
+       * Header
+       * - Logo
+       * - Navbars
+       * - Card
+       * 
+       * body
+       * - search bar
+       * - restraunt list
+       * - food cards
+       * - Images
+       * - Rating
+       * - Cuisines
+       * Footer
+       * - Links
+       * - copyright
+       * **/
+    }
+  )
+}
+
 const root = ReactDom.createRoot(document.getElementById("root"));
 // root.render(container);
 root.render(<HeaderComponent />);
